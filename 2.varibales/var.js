fefea62e142d5775debde3a778ad2bc2a1ff3852
DHEERAJKUMARP <dheerajkumarp777@gmail.com>
2024-10-04 12:38:50 +0530

    variables

---
+++ 2.varibales/var.js
@@ -0,0 +1,8 @@
+let name ="dheeraj  ";
+const id=7;
+
+var college = "siet";  // never use var in js becasue it will change the value if use in somevers esle aslo
+
+
+console.log(name);
+console.table([id,name]);
